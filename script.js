27831052d52e1ecf8f94fe95e01b2c83581671cd
MarioZanin <mariocz5@hotmail.com>
2023-11-15 18:19:58 -0300

    1ª Tentativa de adequar a resposta

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function submitForm() {
+function submitForm(siteKey) {
   var name = document.getElementById('name').value;
   var address = document.getElementById('address').value;
   var neighborhood = document.getElementById('neighborhood').value;
@@ -7,10 +7,10 @@ function submitForm() {
   var phone = document.getElementById('phone').value;
   var message = document.getElementById('message').value;
 
-  // Preencha o espaço '  ' pela sua chave secreta do reCAPTCHA
+  // Substitua 'seu_segredo' pela sua chave secreta do reCAPTCHA
   var recaptchaSecret = '6LeSzBApAAAAAFCNulGKzbAKDPKs0JebR2XJCB6-';
 
-  // Preencha abaixo o espaço '   ' pela sua chave do site reCAPTCHA
+  // Substitua 'reCAPTCHA_site_key' pela sua chave do site reCAPTCHA
   var recaptchaToken;
   grecaptcha.ready(function() {
     grecaptcha.execute('6LeSzBApAAAAAPjsa_dNEHVPZQzx5q4TwBgiQqTf', { action: 'submit' }).then(function(token) {
@@ -29,8 +29,8 @@ function submitForm() {
 
         // Verifique a resposta da API do reCAPTCHA
         if (response.success) {
-          // Se a verificação for bem-sucedida, envie os dados do formulário para a API do seu servidor
-          sendFormDataToServer(name, address, neighborhood, city, state, phone, message, recaptchaToken);
+          // Se a verificação for bem-sucedida, envie os dados do formulário para a nova página
+          displayFormData(name, address, neighborhood, city, state, phone, message);
         } else {
           // Se a verificação falhar, trate de acordo (por exemplo, exiba uma mensagem de erro)
           console.error('Falha na verificação reCAPTCHA');
@@ -42,54 +42,31 @@ function submitForm() {
   });
 }
 
-function sendFormDataToServer(name, address, neighborhood, city, state, phone, message, recaptchaToken) {
-  // Substitua a URL abaixo pela URL da sua API onde você deseja enviar os dados do formulário
-  var apiUrl = 'https://www.google.com/recaptcha/api/siteverify MÉTODO: POST';
+function displayFormData(name, address, neighborhood, city, state, phone, message) {
+  // Crie uma nova página HTML para exibir os dados do formulário
+  var newPage = window.open('');
+  var result = "Nome: " + name + "<br>" +
+               "Endereço: " + address + "<br>" +
+               "Bairro: " + neighborhood + "<br>" +
+               "Cidade: " + city + "<br>" +
+               "Estado: " + state + "<br>" +
+               "Telefone: " + phone + "<br>" +
+               "Mensagem: " + message;
 
-  // Montar os dados do formulário
-  var formData = {
-    name: name,
-    address: address,
-    neighborhood: neighborhood,
-    city: city,
-    state: state,
-    phone: phone,
-    message: message,
-    recaptchaToken: recaptchaToken
-  };
-
-  // Envie os dados para a API do seu servidor
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', apiUrl, true);
-  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      console.log('Formulário enviado com sucesso para o servidor:', xhr.responseText);
-    } else {
-      console.error('Erro ao enviar o formulário para o servidor. Status:', xhr.status);
-    }
-  };
-
-  xhr.send(JSON.stringify(formData));
+  // Exiba os dados na nova página
+  newPage.document.write(result);
 }
 
 function onClick(event) {
-    e.preventDefault();
-    grecaptcha.ready(function() {
-      grecaptcha.execute('6LeSzBApAAAAAPjsa_dNEHVPZQzx5q4TwBgiQqTf', {action: 'submit'}).then(function(token) {
-        
-       var result = "Nome: " + name + "<br>" +
-        "Endereço: " + address + "<br>" +
-        "Bairro: " + neighborhood + "<br>" +
-        "Cidade: " + city + "<br>" +
-        "Estado: " + state + "<br>" +
-        "Telefone: " + phone + "<br>" +
-        "Mensagem: " + message;
+  event.preventDefault();
+  grecaptcha.ready(function() {
+    grecaptcha.execute('6LeSzBApAAAAAPjsa_dNEHVPZQzx5q4TwBgiQqTf', {action: 'submit'}).then(function(token) {
+  
+  // Substitua 'reCAPTCHA_site_key' pela sua chave do site reCAPTCHA
+  //var siteKey = '6LeSzBApAAAAAPjsa_dNEHVPZQzx5q4TwBgiQqTf';
 
-       var newPage = window.open('');
-       newPage.document.write(result);
-     
-      });
-    });
+  // Chame a função submitForm para processar o formulário
+  submitForm(siteKey);
 }
+
+
