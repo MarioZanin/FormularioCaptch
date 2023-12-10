528d1d4b74e2cfcb71c8f95ddfe9af3e66dd45cb
MarioZanin <mariocz5@hotmail.com>
2023-12-10 00:19:20 -0300

    reduz código

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/processar-formulario', async (req, res) => {
         Obrigado por preencher o formulário, ${name}!
 
         Para confirmar sua identidade, clique no link a seguir:
-        http://seusite.com/confirmar?email=${email}&token=${recaptchaToken}
+        https://mariozanin.github.io/FormularioCaptch/processar-formulario?email=${email}&token=${recaptchaToken}
       `;
 
       const mailOptions = {
@@ -69,45 +69,6 @@ app.post('/processar-formulario', async (req, res) => {
 // Rota para confirmar a identidade
 app.post('/confirmar', (req, res) => {
     const { email, token } = req.body;
-
-
-
-
-
-
-
-    // Verifique se o token é válido (pode incluir lógica mais robusta)
-    if (recaptchaToken) {
-      // Envie o e-mail de confirmação para o e-mail fornecido no formulário
-      const emailBody = `
-        Obrigado por preencher o formulário, ${name}!
-  
-        Seu formulário foi recebido. Agora você pode clicar no link a seguir para confirmar:
-        https://mariozanin.github.io/FormularioCaptch/processar-formulario?name=${name}&email=${email}&address=${address}&neighborhood=${neighborhood}&city=${city}&state=${state}&phone=${phone}&message=${message}&recaptchaToken=${recaptchaToken}
-      `;
-  
-      const mailOptions = {
-        from: process.env.EMAIL_SENDER,
-        to: email,
-        subject: 'Confirmação de Identidade',
-        text: emailBody
-      };
-  
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error('Erro ao enviar e-mail de confirmação:', error);
-          res.status(500).send('Erro ao processar o formulário.');
-        } else {
-          console.log('E-mail de confirmação enviado:', info.response);
-          res.status(200).send('E-mail de confirmação enviado com sucesso!');
-        }
-      });
-    } else {
-      console.error('Token inválido.');
-      res.status(400).send('Token inválido.');
-    }
-  });
-  
 
 
   // Rota para processar o formulário após a confirmação
