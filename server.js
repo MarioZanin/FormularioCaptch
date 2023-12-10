b5b4916fb1625c573e75c071224f4967c7998d12
MarioZanin <mariocz5@hotmail.com>
2023-12-09 22:58:02 -0300

    refatora HOTP

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const jwt = require('jsonwebtoken');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const speakeasy = require('speakeasy');
-const dotenv = require('dotenv');
-dotenv.config();
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,85 +11,78 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
-// Dados fictícios de usuário
-const users = [
-    { id: 1, username: 'usuario', password: 'senha_secreta', totpSecret: process.env.TOTP_SECRET }
-];
+// Rota para processar o formulário
+app.post('/processar-formulario', async (req, res) => {
+  const { name, email, address, neighborhood, city, state, phone, message, recaptchaToken } = req.body;
 
-// Rota para renderizar a página de login
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+  // Verifica o reCAPTCHA
+  const recaptchaUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${recaptchaToken}`;
 
-// Rota para processar o formulário de login
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
+  try {
+    const recaptchaResponse = await axios.post(recaptchaUrl);
 
-    if (user) {
-        // Gera um token de autenticação
-        const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    if (recaptchaResponse.data.success) {
+      // Envia o e-mail de confirmação para o e-mail fornecido no formulário
+      const emailBody = `
+        Obrigado por preencher o formulário, ${name}!
 
-        // Gera um segredo TOTP para o usuário, se ainda não tiver
-        if (!user.totpSecret) {
-            user.totpSecret = speakeasy.generateSecret({ length: 20 }).base32;
-        }
+        Para confirmar sua identidade, clique no link a seguir:
+        http://seusite.com/confirmar?email=${email}&token=${recaptchaToken}
+      `;
 
-        // Envia o segredo para o e-mail do usuário
-        const emailBody = `Seu código TOTP: ${user.totpSecret}`;
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: `${username}mariozanin5@gmail.com`, // Substitua pelo e-mail real do usuário
-            subject: 'Código TOTP',
-            text: emailBody
-        };
+      const mailOptions = {
+        from: process.env.EMAIL_SENDER,
+        to: email,
+        subject: 'Confirmação de Identidade',
+        text: emailBody
+      };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Erro ao enviar e-mail:', error);
-                res.status(500).send('Erro ao processar o login.');
-            } else {
-                console.log('E-mail enviado:', info.response);
-                // Renderiza a página para inserir o código TOTP
-                res.sendFile(__dirname + '/totp.html');
-            }
-        });
-    } else {
-        res.status(401).send('Credenciais inválidas.');
-    }
-});
-
-// Rota para verificar o código TOTP
-app.post('/verify-totp', (req, res) => {
-    const { username, totp } = req.body;
-    const user = users.find(u => u.username === username);
-
-    if (user) {
-        // Verifica o código TOTP
-        const verified = speakeasy.totp.verify({
-            secret: user.totpSecret,
-            encoding: 'base32',
-            token: totp,
-            window: 1
-        });
-
-        if (verified) {
-            res.status(200).send('Código TOTP válido. Login bem-sucedido!');
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Erro ao enviar e-mail de confirmação:', error);
+          res.status(500).send('Erro ao processar o formulário.');
         } else {
-            res.status(401).send('Código TOTP inválido.');
+          console.log('E-mail de confirmação enviado:', info.response);
+          // Agora, você pode decidir se deseja processar o formulário imediatamente ou aguardar a confirmação
+          res.status(200).send('E-mail de confirmação enviado com sucesso!');
         }
+      });
     } else {
-        res.status(401).send('Usuário não encontrado.');
+      console.error('Falha na verificação reCAPTCHA');
+      res.status(400).send('Falha na verificação reCAPTCHA.');
     }
+  } catch (error) {
+    console.error('Erro na verificação reCAPTCHA:', error.message);
+    res.status(500).send('Erro ao processar o formulário.');
+  }
+});
+
+// Rota para confirmar a identidade
+app.get('/confirmar', (req, res) => {
+  const { email, token } = req.query;
+
+  // Verifica se o token é válido (pode incluir lógica mais robusta)
+  if (token) {
+    // Aqui você pode adicionar lógica adicional se necessário antes de processar o formulário
+    // ...
+
+    // Processa o formulário
+    res.status(200).send('Identidade confirmada. Formulário processado com sucesso!');
+  } else {
+    console.error('Token inválido.');
+    res.status(400).send('Token inválido.');
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
